99cbfc51c3c624f382ba50dc6aed6224de6a878c
irabbi360 <fazle08rabbi@gmail.com>
2018-02-25 00:35:43 +0600

    Meteor vue js setup done

---
+++ imports/ui/footer.vue
@@ -0,0 +1,25 @@
+<template>
+	<div id="footer">
+		<div class="footers">
+		  <p>Footer</p>
+		</div>
+	</div>
+	
+</template>
+
+<script>
+	export default {
+	}
+</script>
+
+<style>
+.footers {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: darkgrey;
+   color: white;
+   text-align: center;
+}
+</style>
